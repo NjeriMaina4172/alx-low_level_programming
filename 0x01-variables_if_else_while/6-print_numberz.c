25f330e5758281fb93be4ef4d6413ddf9f6adc1b
NjeriMaina4172 <lucymaina4172@gmail.com>
2022-09-09 08:44:54 -0700

    9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print numbers between 0 and 9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
