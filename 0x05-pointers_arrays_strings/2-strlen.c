5e7e516104af67cb716f720a456775e1ebca7aeb
NjeriMaina4172 <lucymaina4172@gmail.com>
2022-09-19 06:47:18 -0700

    length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - returns length
+ * @s: input parameter
+ *
+ * Description: uses pointers
+ * Return: Always 0
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+		continue;
+	}
+
+	return (i);
+}
