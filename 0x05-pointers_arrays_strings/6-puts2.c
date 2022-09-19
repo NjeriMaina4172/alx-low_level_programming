4186944b22347cd764777c08affe937e6ea7a119
NjeriMaina4172 <lucymaina4172@gmail.com>
2022-09-19 13:17:40 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 - print character
+ * @str: input string
+ *
+ * Description: prints character of a string, starting first
+ * character
+ * Return: Always (0)
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
