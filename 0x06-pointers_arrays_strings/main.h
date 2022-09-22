9e99f3967e4a3ba72e19f83a15fb9316d0c7dd56
NjeriMaina4172 <lucymaina4172@gmail.com>
2022-09-22 07:31:58 -0700

    mozart

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,3 +9,5 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+char *leet(char *);
+int _putchar(char c);
