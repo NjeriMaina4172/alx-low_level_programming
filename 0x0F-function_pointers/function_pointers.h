aee315210aa9ab33834a3b7543fc4bc7c5cecd08
NjeriMaina4172 <lucymaina4172@gmail.com>
2022-10-13 23:09:54 -0700

    protected

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,11 +1,15 @@
-#ifndef _FUNCTION_POINTERS_H
-#define _FUNCTION_POINTERS_H_
+#ifndef POINTERFUNCS
 
-#include <stddef.h>
+#define POINTERFUNCS
+
+
+#include <stdio.h>
+
+#include <stdlib.h>
 
 int _putchar(char);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
-#endif
+#endif /* POINTERFUNCS*/
